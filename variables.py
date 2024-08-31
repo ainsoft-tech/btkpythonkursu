debecf764ebea2acd0817e527b8eeb9836f88dd6
Ali İhsan NASLI <aliihsannasli@hotmail.com>
2024-08-31 12:52:11 +0300

    tip dönüşümleri dosyası eklendi

--- variables.py
+++ variables.py
@@ -4,3 +4,31 @@ vergi = 0.27
 
 print(maasAli-(maasAli * vergi))
 print(maasAhmet-(maasAhmet * vergi))
+
+# Değişken tanımlama kuralları
+
+# değişken rakam ile başlayamaz.
+# değişken tanımlarken büyük küçük harf duyarlılığı vardır.
+# türkçe karakter kullanılmaz.
+# değişkenler arasında boşluk olamaz.
+
+x = 1
+y = 2.3
+name = "Çınar"
+surname = "Yılmaz"
+isStudent = True
+
+"""
+x, y, name, isStudent = (1, 2.3, 'Çınar', True) şeklinde yukarıdaki 4 veriyi tek satırda yazabiliriz.
+üç tırnak işareti çoklu yorum satırı için kullanılır. 
+"""
+
+print(name + " " + surname)
+a = 10
+b = 20
+print(a + b)
+
+a = '10'
+b = '20'
+print(a + b)
+
