3f299322e797b7f09d0ce1dfd22df3f12a6d0378
Ali İhsan NASLI <aliihsannasli@hotmail.com>
2024-09-05 16:38:52 +0300

    programın örnek kodları eklendi

--- index.py
+++ index.py
@@ -1,9 +1,9 @@
-users = {
-    'sadikturan':{
-        'age': 36,
-        'email': 'sadik@gmail.com',
-        'address': 'Kocaeli',
-        'phone': '1212531'
-    }
-}
-print(users)
+x = int(input('x= '))
+y = int(input('y= '))
+
+if (x > y):
+    print('x y den büyüktür')
+elif (x == y ):
+    print ('x ve y birbirine eşittir')
+else:
+    print('y x ten büyüktür')
