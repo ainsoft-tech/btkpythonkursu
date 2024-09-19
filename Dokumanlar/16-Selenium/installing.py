c982119e5d2b2d0111fa7efe52e230ec14e98d7d
Ali İhsan NASLI <aliihsannasli@hotmail.com>
2024-09-19 16:45:28 +0300

    selenium için edgedriver eklendi

--- Dokumanlar/16-Selenium/installing.py
+++ Dokumanlar/16-Selenium/installing.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
 
 # driver = webdriver.Chrome()
-driver = webdriver.Firefox()
-
+# driver = webdriver.Firefox()
+driver = webdriver.Edge()
 url = "http://sadikturan.com"
 
 driver.get(url)
