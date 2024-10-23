de90f556189a4ed91e088a4f11b72ac9f5634c15
Ali İhsan <aliihsannasli@hotmail.com>
2024-10-23 17:34:40 +0300

    Movies uygulama dosyaları eklendi.

--- Dokumanlar/24-Web Geliştirme Django/catalog/catalog/urls.py
+++ Dokumanlar/24-Web Geliştirme Django/catalog/catalog/urls.py
@@ -21,5 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('pages.urls')),
+    path('movies/', include('movies.urls')),
     path('admin/', admin.site.urls),
+
 ]
---
+++ Dokumanlar/24-Web Geliştirme Django/catalog/movies/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Movie(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Film Adı')
+    description = models.TextField(verbose_name='Açıklama')
+    image = models.CharField(max_length=100, verbose_name='Film Afişi')
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name='Eklenme Tarihi')
+    isPublished = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
